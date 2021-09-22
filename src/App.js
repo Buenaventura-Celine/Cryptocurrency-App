c633a76f0ff5a351719145180d361f1a00e48710
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-22 12:00:20 +0800

    Main panel created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
 
-import { Navbar } from './components'
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components'
 import './App.css'
 
 const App = () => {
@@ -14,7 +14,17 @@ const App = () => {
             </div>
 
             <div className="main">
-
+                <Layout>
+                    <div className="routes">
+                        <Switch>
+                            <Route exact path="/"><Homepage/></Route>
+                            <Route exact path="/exchanges"><Exchanges/></Route>
+                            <Route exact path="/cryptocurrencies"><Cryptocurrencies/></Route>
+                            <Route exact path="/crypto/:id"><CryptoDetails/></Route>
+                            <Route exact path="/news"><News/></Route>
+                        </Switch>
+                    </div>
+                </Layout>
             </div>
 
             <div className="footer">
