7e1f558c82f16f7695dbb5331268f42a95a19d0d
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-22 09:44:33 +0800

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+
+import { Navbar } from './components'
 
 const App = () => {
     return (
-        <div>
-            Crypto World
+        <div className="app">
+
+            <div className="navbar">
+                <Navbar/>
+            </div>
+
+            <div className="main">
+
+            </div>
+
+            <div className="footer">
+
+            </div>
+
         </div>
     )
 }
