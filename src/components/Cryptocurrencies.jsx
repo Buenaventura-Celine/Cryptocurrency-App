6b73d1fea709b797acf8fb79bad6a6165927ee6f
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-23 10:52:27 +0800

    Hide and show the search bar

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -21,9 +21,12 @@ const Cryptocurrencies = ({ simplified }) => {
 
   return (
     <>
-      <div className="search-crypto">
-        <Input placeholder="Search crptocurrency" onChange={(e) => setSearchTerm(e.target.value)}/>
-      </div>
+      {!simplified && (
+         <div className="search-crypto">
+         <Input placeholder="Search crptocurrency" onChange={(e) => setSearchTerm(e.target.value)}/>
+       </div>
+      )}
+     
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
